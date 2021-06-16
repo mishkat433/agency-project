5dab431e15dbfee22cb74390ba75c13e675036ac
Mishkat <mishkat433@gmail.com>
2021-06-16 16:39:08 +0600

    changed

---
+++ js/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(".slider-area").owlCarousel({
+        items:1,
+        loop:true,
+        nav:true,
+        autoplay:true
+    });
+
+    $(".testomonials").owlCarousel({
+        items:3,
+        loop:true,
+        nav:true,
+        autoplay: true,
+        center: true
+    });
+    
+    $('.counter').counterUp();
+
+
+
+});
+
+
+$(window).on("load" ,function(){
+    $(".preloader").fadeOut(1000);
+})
---
+++ js/test.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+
+$(document).ready(function(){
+
+    // $(window).scroll(function(){
+    //     if($(this).scrollTop() > 100){
+    //         $("#top").fadeIn(1000)
+    //     }
+    //     else{
+    //         $("#top").fadeOut()
+    //     }
+    // })
+
+   $("#top").click(function(){
+    $("html, body").animate({
+        scrollTop:0
+    },2000);
+   })
+
+})
